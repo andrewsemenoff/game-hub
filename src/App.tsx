6d52c4363eb6b03bd460419dfbd10cc1b99732ae
andrewsemenoff <107771959+andrewsemenoff@users.noreply.github.com>
2023-10-26 14:40:37 +0300

    Build platform selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
-import NavBar from "./components/NavBar";
+import { useState } from "react";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
-import { useState } from "react";
+import NavBar from "./components/NavBar";
 import { Genre } from "./hooks/useGenres";
+import PlatformSelector from "./components/PlatformSelector";
 
 function App() {
-  const [selectedGenre, setSelectedGenre] = useState<Genre|null>(null);
+  const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
   const handleSelectGenre = (genre: Genre) => {
     setSelectedGenre(genre);
   };
@@ -20,11 +21,15 @@ function App() {
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" paddingX={5}>
-          <GenreList onSelectGenre={handleSelectGenre} selectedGenre={selectedGenre}/>
+          <GenreList
+            onSelectGenre={handleSelectGenre}
+            selectedGenre={selectedGenre}
+          />
         </GridItem>
       </Show>
       <GridItem area="main">
-        <GameGrid selectedGenre={selectedGenre}/>
+        <PlatformSelector />
+        <GameGrid selectedGenre={selectedGenre} />
       </GridItem>
     </Grid>
   );
