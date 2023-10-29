2d530dfb5bb1411c5fa50430a2ef9c4cb0e4cf10
andrewsemenoff <107771959+andrewsemenoff@users.noreply.github.com>
2023-10-29 15:24:31 +0200

    Update selector names

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -5,7 +5,7 @@ const sortOrders = [
   { value: "", label: "Relevance" },
   { value: "-added", label: "Date added" },
   { value: "name", label: "Name" },
-  { value: "-released", label: "Release date" },
+  { value: "-released", label: "Release date " },
   { value: "-metacritic", label: "Popularity" },
   { value: "-rating", label: "Average rating" },
 ];
